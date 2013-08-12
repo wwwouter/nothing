80314619e842161596158418705209eef23a0053
wwwouter <wouter@wercker.com>
2013-08-12 14:19:23 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+export log="`sudo apt-get install lxc -y`"
+echo "apt-get done:"
+echo $log
+sudo stop lxc
+sudo sed -i "s/10.0.3/10.0.4/g" /etc/default/lxc
+sudo start lxc
+sudo apt-get install xz-utils -y
+sudo apt-get install software-properties-common -y
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:dotcloud/lxc-docker -y
+sudo apt-get update
+sudo apt-get install lxc-docker -y
